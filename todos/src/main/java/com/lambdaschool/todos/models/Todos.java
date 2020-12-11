9db2e27e83c1f35e54443fe95a40f25b202fe3e6
Scott Allen <70392745+sallen95@users.noreply.github.com>
2020-12-11 09:53:27 -0700

    Most endpoints are working

---
+++ todos/src/main/java/com/lambdaschool/todos/models/Todos.java
@@ -0,0 +1,79 @@
+package com.lambdaschool.todos.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todos extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "userid",
+        nullable = false)
+    @JsonIgnoreProperties(value = "todos", allowSetters = true)
+    private User user;
+
+    public Todos()
+    {
+        this.completed = false;
+    }
+
+    public Todos(
+        User user,
+        String description)
+    {
+        this.description = description;
+        this.user = user;
+    }
+
+    public long getTodoid()
+    {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid)
+    {
+        this.todoid = todoid;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public boolean isCompleted()
+    {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed)
+    {
+        this.completed = completed;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+}
